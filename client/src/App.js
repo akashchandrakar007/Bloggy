037f7dbdf8b2c50feded072333a4c31638d2990a
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2022-04-05 14:18:58 +0530

    Intial setup for client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopBar from './components/topbar/TopBar';
+const App=()=>{
+  return (
+    <>
+    <TopBar />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/topbar/TopBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./topbar.css";
+
+const TopBar=()=>{
+  return (
+     <div className="top">
+       topbar
+       
+     </div>
+  );
+};
+
+export default TopBar;
