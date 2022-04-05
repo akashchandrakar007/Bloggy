3a3d7c321fa2b92eb7018fa1347cd6dea5af7172
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2022-04-05 18:12:50 +0530

    topbar finished

--- client/src/components/topbar/TopBar.js
+++ client/src/components/topbar/TopBar.js
@@ -4,8 +4,27 @@ import "./topbar.css";
 const TopBar=()=>{
   return (
      <div className="top">
-       topbar
-       
+        <div className="topLeft">
+           <i className="topIcon fa-brands fa-facebook-square"></i>
+           <i className="topIcon fa-brands fa-instagram-square"></i>
+           <i className="topIcon fa-brands fa-twitter-square"></i>
+           <i className="topIcon fa-brands fa-youtube-square"></i>
+        </div>
+        <div className="topCenter">
+         <ul className="topList">
+            <li className="topListItem">HOME</li>
+            <li className="topListItem">ABOUT</li>
+            <li className="topListItem">CONTACT</li>
+            <li className="topListItem">WRITE</li>
+            <li className="topListItem">LOGOUT</li>
+         </ul>
+        </div>
+        <div className="topRight">
+          <img className="topImg" src="https://staticg.sportskeeda.com/editor/2021/10/5596d-16331824813563-800.jpg" alt=""/>
+          <i className="searchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
+
+
      </div>
   );
 };
